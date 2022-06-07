96ec4ca5e6a681b01b3333d9c94e6ad557f0962b
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-07 20:31:08 +0300

    added the profile function

--- clone/views.py
+++ clone/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 # Create your views here.
@@ -13,7 +14,11 @@ def register(request):
             form.save() # Save user to Database
             username = form.cleaned_data.get('username') # Get the username that is submitted
             messages.success(request, f'Account created for {username}!') # Show sucess message when account is created
-            return redirect('welcome.html') # Redirect user to Homepage
+            return redirect('login') # Redirect user to Homepage
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
