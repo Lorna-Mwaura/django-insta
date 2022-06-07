dc531c88e4af42d430405a257f7c093cda40e6a9
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-07 20:25:53 +0300

    added the urls for login,logout,profile

--- clone/urls.py
+++ clone/urls.py
@@ -1,7 +1,11 @@
 from django.urls import re_path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
     re_path('^$',views.welcome,name='welcome'),
-    re_path('register/', views.register, name='register')
+    re_path('register/', views.register, name='register'),
+    re_path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    re_path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    re_path('profile/', views.profile, name='profile')
 ]
