2e6fdd8fc457f35de35547d43e471c4039b115f6
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-07 17:05:36 +0300

    created the register view

--- clone/views.py
+++ clone/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
 def welcome(request):
     return render(request,"welcome.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST) 
+        if form.is_valid():
+            form.save() # Save user to Database
+            username = form.cleaned_data.get('username') # Get the username that is submitted
+            messages.success(request, f'Account created for {username}!') # Show sucess message when account is created
+            return redirect('welcome.html') # Redirect user to Homepage
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
