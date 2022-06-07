41b8cf77a615e9079572e35fe53903c15e38031c
Lorna-Mwaura <lornamwaura99@gmail.com>
2022-06-07 14:52:35 +0300

    added the like and comment models

--- clone/models.py
+++ clone/models.py
@@ -10,8 +10,39 @@ class Image(models.Model):
     caption =  models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, related_name = 'created_at', on_delete=models.CASCADE, null=True, blank=True)
+    likes = models.ForeignKey(User, related_name='liked_by', on_delete=models.CASCADE,blank=True, null=True)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE, blank = True, null=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = CloudinaryField('image')
     bio = models.TextField()
+
+class Like(models.Model):
+    like= models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_liked = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return str(self.like)
+
+class Comment(models.Model):
+    message= models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True,null=True)
+    image_id = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def add_comment(self):
+        self.save()
+
+    class Meta:
+        ordering = ['created_on']
+
+    def get_post_comments(cls, image_id):
+        comments = cls.objects.filter(image_id=image_id)
+        return comments
+
+    @classmethod
+    def get_comments(cls):
+        comments=cls.objects.all()
+        return comments
